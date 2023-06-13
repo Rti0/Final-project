f522e754054b851c2b812588c2adf6771284b1f4
Reem Ibrahim <118621764+Rti0@users.noreply.github.com>
2023-06-13 23:20:30 +0300

    Add files via upload

---
+++ Desiner/src/main/java/com/example/desiner/Service/RateOrder.java
@@ -0,0 +1,9 @@
+package com.example.desiner.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RateOrder {
+}
